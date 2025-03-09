149f9d45735a418405a669e40486ca69c9c47f38
mbirochan <mbirochan@gmail.com>
2025-03-08 18:27:01 -0600

    Initial commit with new structure and project plan

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: "class", // Ensure this is set
+    content: ["./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
