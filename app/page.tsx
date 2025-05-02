5624d54f71885164890b7953b68eb4a2c1dc9f3a
mbirochan <mbirochan@gmail.com>
2025-05-02 17:36:00 -0500

    added Escrow account project

--- app/page.tsx
+++ app/page.tsx
@@ -120,6 +120,11 @@ export default function Home() {
       link: 'https://github.com/mbirochan'
     },
     {
+      title: 'Escrow Daap with AI agents for decision making and contract drafting',
+      description: 'I built an escrow daap with AI agents for decision making and contract drafting.',
+      link: 'https://github.com/mbirochan'
+    },
+    {
       title: 'Blog Site with React Native',
       description: 'A blog site built with React Native and Firebase for real-time data storage. I post my blogs here.',
       link: 'https://github.com/mbirochan'
@@ -129,11 +134,7 @@ export default function Home() {
       description: 'A personal portfolio website built with Next.js and Tailwind CSS.',
       link: 'https://github.com/mbirochan'
     },
-    {
-      title: 'Blog Site with React',
-      description: 'A blog site built with React and Firebase for real-time data storage. I post my blogs here.',
-      link: 'https://github.com/mbirochan'
-    },
+
   ];
 
   const sections = [
